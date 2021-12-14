29b12b5f528c5106826f44dcb9715fdbdf473f99
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-12-13 21:54:30 -0500

    Create keyboard
    Added an empty div with the id of "keyboard."
    
    Created the function createKeyboard.
    
    Used let to create a string for all the letters of the alphabet.
    
    Split this string up into individual letters, each with the id of "letter."
    
    Joined them together with nothing.
    
    Set the inner HTML of the keyboard to the letters.

--- page.js
+++ page.js
@@ -18,3 +18,19 @@ let maxWrong = 6;
 function pickWord() {
   answer = words[Math.floor(Math.random() * words.length)];
 }
+
+function createKeyboard() {
+  let buttons = "abcdefghijklmnopqrstuvwxyz".split("").map(letter =>
+    `
+      <button
+        id="letter"
+        onClick="handleGuess("letter")"
+      >
+        ` + letter + `
+      </button>
+    `).join("");
+  document.getElementById("keyboard").innerHTML = buttons;
+}
+
+pickWord();
+createKeyboard();
