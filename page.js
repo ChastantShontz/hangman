8aa7c5c2518fe6b28ef2a896ed98688dda036329
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-12-16 19:48:18 -0500

    Clean up
    Replaced the empty div .keyboard with individual divs for each letter button.
    
    Got rid of the function createKeyboard.
    
    Put only two words in the array words.
    
    Added the function pickLetter in all the letter buttons onclick.
    
    This function finds the index of the chosen letter and adds it to the anonymous word if it exists.

--- page.js
+++ page.js
@@ -1,13 +1,6 @@
 var words = [
   "bat",
-  "cat",
-  "fat",
-  "hat",
-  "mat",
-  "pat",
-  "rat",
-  "sat",
-  "vat"
+  "man"
 ];
 
 let answer = "";
@@ -18,25 +11,23 @@ function pickWord() {
   answer = words[Math.floor(Math.random() * words.length)];
 }
 
-function createKeyboard() {
-  let buttons = "abcdefghijklmnopqrstuvwxyz".split("").map(letter =>
-    `
-      <button
-        id="letter"
-        onClick="handleGuess("letter")"
-      >
-        ` + letter + `
-      </button>
-    `).join("");
-  document.getElementById("keyboard").innerHTML = buttons;
+function pickLetter(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+  if (answer.indexOf(chosenLetter) >= 0) {
+    wordGuess();
+    checkIfGameWon();
+  }
+  else if (answer.indexOf(chosenLetter) === -1) {
+    wrong++;
+  }
 }
 
 function wordGuess() {
-  wordGuess = answer.split("").map(letter =>
+  chosenWord = answer.split("").map(letter =>
     (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-  document.getElementById('unknownWord').innerHTML = wordGuess;
+  document.getElementById('unknownWord').innerHTML = chosenWord;
 }
 
 pickWord();
-createKeyboard();
 wordGuess();
