52453f5e5840e39565e94e36e10f0530ba58494f
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-12-16 22:35:55 -0500

    You won
    Created the function youWon to go inside the else if loop in the function pickLetter.
    
    This function triggers an alert telling you that you won if the variable chosenWord is equal to the answer.

--- page.js
+++ page.js
@@ -16,6 +16,7 @@ function pickLetter(chosenLetter) {
   document.getElementById(chosenLetter).setAttribute("disabled", true);
   if (answer.indexOf(chosenLetter) >= 0) {
     wordGuess();
+    youWon();
   }
   else if (answer.indexOf(chosenLetter) === -1) {
     wrong++;
@@ -27,6 +28,12 @@ function wordGuess() {
   chosenWord = answer.split("").map(letter =>
     (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
   document.getElementById("unknownWord").innerHTML = chosenWord;
+}
+
+function youWon() {
+  if (chosenWord === answer) {
+    alert("You Won!");
+  }
 }
 
 pickWord();
