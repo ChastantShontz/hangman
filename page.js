71449b870738a9973ce21583609102a412c759a2
ChastantShontz <78157527+ChastantShontz@users.noreply.github.com>
2021-12-16 23:19:10 -0500

    Reset
    Created the function reset and put it inside the reset button onclick.
    
    This function sets the variable guessed back to an empty array.
    
    It sets the variable wrong back to 0.
    
    It sets the inner HTML of the span .wrong equal to the variable wrong.
    
    It runs the functions pickWord and wordGuess.
    
    It removes the disabled attribute from all the letter buttons.

--- page.js
+++ page.js
@@ -11,35 +11,48 @@ function pickWord() {
   answer = words[Math.floor(Math.random() * words.length)];
 }
 
-function pickLetter(chosenLetter) {
-  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
-  document.getElementById(chosenLetter).setAttribute("disabled", true);
-  if (answer.indexOf(chosenLetter) >= 0) {
-    wordGuess();
-    youWon();
-  }
-  else if (answer.indexOf(chosenLetter) === -1) {
-    wrong++;
-    document.getElementById("wrong").innerHTML = wrong;
-    youLost();
-  }
-}
-
 function wordGuess() {
   chosenWord = answer.split("").map(letter =>
     (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
   document.getElementById("unknownWord").innerHTML = chosenWord;
 }
 
+function pickLetter(chosenLetter) {
+  guessed.indexOf(chosenLetter) == -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).disabled = true;
+  if (answer.indexOf(chosenLetter) >= 0) {
+    wordGuess();
+    youWon();
+  }
+  else if (answer.indexOf(chosenLetter) == -1) {
+    wrong++;
+    document.getElementById("wrong").innerHTML = wrong;
+    youLost();
+  }
+}
+
 function youWon() {
-  if (chosenWord === answer) {
+  if (chosenWord == answer) {
     alert("You Won!");
   }
 }
 
 function youLost() {
-  if (wrong === 6) {
+  if (wrong == 6) {
     alert("You Lost...");
+  }
+}
+
+function reset() {
+  guessed = [];
+  wrong = 0;
+  document.getElementById("wrong").innerHTML = wrong;
+  pickWord();
+  wordGuess();
+  let enable = document.getElementsByClassName("letter");
+  var rep;
+  for (rep = 0; rep < enable.length; rep++) {
+    enable[rep].removeAttribute("disabled");
   }
 }
 
